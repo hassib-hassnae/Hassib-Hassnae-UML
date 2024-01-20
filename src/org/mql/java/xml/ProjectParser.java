b066083369b36d38f08e2d197ef842c893e76930
hassib-hassnae <154868723+hassib-hassnae@users.noreply.github.com>
2024-01-20 01:13:31 +0100

    Fifth Commit

---
+++ src/org/mql/java/xml/ProjectParser.java
@@ -0,0 +1,69 @@
+package org.mql.java.xml;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.mql.java.xml.XmlNode;
+import org.mql.java.explorer.ExplorerProject;
+
+public class ProjectParser {
+	
+	public ProjectParser() {
+		parse("resources/projet.xml");
+		
+	}
+	
+	public void parse(String source) {
+	    XmlNode root = new XmlNode(source);
+	    String projectName = root.getAttribute("name");
+	    System.out.println("Project : " + projectName);
+
+	    for (int i = 0; i < root.getChildren().length; i++) {
+	        System.out.println("\t Package: " + root.getChildren()[i].getAttribute("name"));
+
+	        for (int j = 0; j < root.getChildren()[i].getChildren().length; j++) {
+	            System.out.println("\t\t Classe: " + root.getChildren()[i].getChildren()[j].getAttribute("name"));
+
+	            // Afficher les méthodes
+	            XmlNode methodsNode = root.getChildren()[i].getChildren()[j].getChild("methods");
+	            if (methodsNode != null) {
+	                XmlNode[] methods = methodsNode.getChildren();
+	                    for (int k = 0; k < methods.length; k++) {
+	                        XmlNode methodNode = methods[k];
+	                        if (methodNode != null) {
+	                            System.out.println("\t\t\t Method : " + methodNode.getAttribute("name"));
+	                        }
+	                    }
+	               
+	            } else {
+	                System.out.println("\t\t\t No methods within this class");
+	            }
+
+	            // Afficher les champs
+	            XmlNode fieldsNode = root.getChildren()[i].getChildren()[j].getChild("fields");
+	            if (fieldsNode != null) {
+	                XmlNode[] fields = fieldsNode.getChildren();
+	                    for (int k = 0; k < fields.length; k++) {
+	                        XmlNode fieldNode = fields[k];
+	                        if (fieldNode != null) {
+	                            System.out.println("\t\t\t field : " + fieldNode.getAttribute("name"));
+	                        }
+	                    }
+	               
+	            } else {
+	                System.out.println("\t\t\t No fields within this class");
+	            }
+
+	        }
+	    }
+
+	}
+
+
+	
+	public static void main(String[] args) {
+		new ProjectParser();
+		
+	}
+
+}
